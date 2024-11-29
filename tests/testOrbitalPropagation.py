c5c585d15d4a5d65404af3bbc8951b712d5ce21d
Cyriaque Guillot <Cyriaque.Guillot@estaca.eu>
2024-11-29 13:35:07 +0100

    Source and test codes are in distincts folders

---
+++ tests/testOrbitalPropagation.py
@@ -0,0 +1,10 @@
+from orbitalPropagation import *
+
+def test_Rot():
+    assert np.linalg.det(Rot(0, 0) == 1
+    assert np.linalg.det(Rot(0, 1) == 1
+    assert np.linalg.det(Rot(0, 2) == 1
+
+def test_radius():
+    a = 6900e3
+    assert radius(a, 0, 0) == a
